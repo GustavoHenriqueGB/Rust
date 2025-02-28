3d17d7cee95d6725d92cb8fa3f699ffe7fd81194
Gustavo Henrique <Gustavohgbzxc123@gmail.com>
2025-02-28 11:26:57 -0300

    Rust introducing

---
+++ hello_cargo/src/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+    println!("Hello, Cargo, Test!");
+    println!("Learning Rust!!!");
+}
---
+++ teste/src/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+    println!("Hello, world!");
+    println!("Testing on Linux!");
+}
